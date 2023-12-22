4fd489c0c1536044bbc4515222c7cb26c98189a6
Takeosenpai <yassine.mou122@gmail.com>
2023-12-22 17:16:31 -0500

    Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
-#!/usr/bin/pup
+#!/usr/bin/puppet
 
 # Install a specific version of Flask (2.1.0)
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+  require  => Exec['update_pip'], # Assuming you need to update pip first
+}
+
+exec { 'update_pip':
+  command => 'pip3 install --upgrade pip',
+  path    => ['/usr/bin'],
 }
